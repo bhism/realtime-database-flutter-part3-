e6ccb59b58d6543eedddd76eddb17f42de9e07e8
BHISM <61289719+bhism@users.noreply.github.com>
2022-03-31 12:40:47 +0530

    Add files via upload

---
+++ addnote.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class addnote extends StatefulWidget {
+  @override
+  _addnoteState createState() => _addnoteState();
+}
+
+class _addnoteState extends State<addnote> {
+  TextEditingController second = TextEditingController();
+
+  TextEditingController third = TextEditingController();
+
+  final fb = FirebaseDatabase.instance;
+  @override
+  Widget build(BuildContext context) {
+    var rng = Random();
+    var k = rng.nextInt(10000);
+
+    final ref = fb.ref().child('todos/$k');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Todos"),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: second,
+                decoration: InputDecoration(
+                  hintText: 'title',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: third,
+                decoration: InputDecoration(
+                  hintText: 'sub title',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              color: Colors.indigo[900],
+              onPressed: () {
+                ref.set({
+                  "title": second.text,
+                  "subtitle": third.text,
+                }).asStream();
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => Home()));
+              },
+              child: Text(
+                "save",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,203 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+
+import 'addnote.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "todo app",
+      theme: ThemeData(
+        primaryColor: Colors.greenAccent[700],
+      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final fb = FirebaseDatabase.instance;
+  TextEditingController second = TextEditingController();
+
+  TextEditingController third = TextEditingController();
+  var l;
+  var g;
+  var k;
+  @override
+  Widget build(BuildContext context) {
+    final ref = fb.ref().child('todos');
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo[900],
+        onPressed: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => addnote(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Todos',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: FirebaseAnimatedList(
+        query: ref,
+        shrinkWrap: true,
+        itemBuilder: (context, snapshot, animation, index) {
+          var v =
+              snapshot.value.toString(); // {subtitle: webfun, title: subscribe}
+
+          g = v.replaceAll(
+              RegExp("{|}|subtitle: |title: "), ""); // webfun, subscribe
+          g.trim();
+
+          l = g.split(','); // [webfun,  subscribe}]
+
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                k = snapshot.key;
+              });
+
+              showDialog(
+                context: context,
+                builder: (ctx) => AlertDialog(
+                  title: Container(
+                    decoration: BoxDecoration(border: Border.all()),
+                    child: TextField(
+                      controller: second,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'title',
+                      ),
+                    ),
+                  ),
+                  content: Container(
+                    decoration: BoxDecoration(border: Border.all()),
+                    child: TextField(
+                      controller: third,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'sub title',
+                      ),
+                    ),
+                  ),
+                  actions: <Widget>[
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                      color: Color.fromARGB(255, 0, 22, 145),
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    MaterialButton(
+                      onPressed: () async {
+                        await upd();
+                        Navigator.of(ctx).pop();
+                      },
+                      color: Color.fromARGB(255, 0, 22, 145),
+                      child: Text(
+                        "Update",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  tileColor: Colors.indigo[100],
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Color.fromARGB(255, 255, 0, 0),
+                    ),
+                    onPressed: () {
+                      ref.child(snapshot.key!).remove();
+                    },
+                  ),
+                  title: Text(
+                    l[1],
+                    // 'dd',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    l[0],
+                    // 'dd',
+
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  upd() async {
+    DatabaseReference ref1 = FirebaseDatabase.instance.ref("todos/$k");
+
+// Only update the name, leave the age and address!
+    await ref1.update({
+      "title": second.text,
+      "subtitle": third.text,
+    });
+    second.clear();
+    third.clear();
+  }
+}
